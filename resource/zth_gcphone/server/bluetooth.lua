9cee9892c4279c6deedb910386b74162fb88469e
zThundy <antonioantonelli7@gmail.com>
2021-06-05 18:48:29 +0200

    ^^^^^

--- resource/zth_gcphone/server/bluetooth.lua
+++ resource/zth_gcphone/server/bluetooth.lua
@@ -1,6 +1,8 @@
 local USERS_BLUETOOTH = {}
 
 gcPhoneT.bluetooth_sendPicToUser = function(data)
+    if not data.userid then return end
+    if not data.link then return end
     local player = source
     TriggerClientEvent("esx:showNotification", player, Config.Language["BLUETOOTH_PICTURE_SENT_OK"])
     TriggerClientEvent("gcphone:bluetooth_receivePic", data.userid, data.link)
