8c3204855a80e55ccda5cb95c436b9d0b3847de2
Gabriele <gabriele.aloise.2003@gmail.com>
2021-07-04 14:42:50 +0000

    Update AppelsActive.vue

--- src/components/Appels/AppelsActive.vue
+++ src/components/Appels/AppelsActive.vue
@@ -18,23 +18,14 @@
     -->
 
     <div class="actionbox">
-      <div class="action raccrocher" :class="{ disableTrue: status === 0 && select !== 0 }">
-        <svg viewBox="0 0 24 24">
-          <g transform="rotate(135, 12, 12)">
-            <path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"/>
-          </g>
-        </svg>
+      <div class="action hangup" :class="{ disableTrue: status === 0 && select !== 0 }">
+            <i class="fas fa-phone"></i>
       </div>
-
-      <div class="action deccrocher" v-if="status === 0" :class="{ disableFalse: status === 0 && select !== 1 }">
-        <svg viewBox="0 0 24 24">
-          <g transform="rotate(0, 12, 12)">
-            <path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"/>
-          </g>
-        </svg>
+      <div class="action answer" v-if="status === 0" :class="{ disableFalse: status === 0 && select !== 1 }">
+          <i class="fas fa-phone"></i>
       </div>
+    </div>
 
-     </div>
    </div>
 </template>
 
@@ -221,16 +212,34 @@ export default {
   border-radius: 50%;
 }
 
-.raccrocher {
+.hangup {
+  text-align: center;
   background-color: #fd3d2e;
+  border-radius: 50px;
   height: 80px;
   width: 80px;
 }
 
-.deccrocher {
+.hangup i {
+  color: #ffffff;
+  font-size: 40px;
+  padding-bottom: 17px;
+  padding-right: 17px;
+  transform: rotate(-135deg);
+}
+
+.answer {
+  text-align: center;
   background-color: #4ddb62;
+  border-radius: 50px;
   height: 80px;
   width: 80px;
+}
+
+.answer i {
+  color: #ffffff;
+  font-size: 40px;
+  padding-top: 20px;
 }
 
 .disableTrue { 
