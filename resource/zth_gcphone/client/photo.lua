40aa2ba198fffaa748e22a61742479de534202a7
zThundy <antonioantonelli7@gmail.com>
2021-09-05 14:27:52 +0200

    added hiding of all components while active camera

--- resource/zth_gcphone/client/photo.lua
+++ resource/zth_gcphone/client/photo.lua
@@ -1,4 +1,29 @@
-frontCam = false
+local HUD_ELEMENTS = {
+    HUD = 0,
+    HUD_WANTED_STARS = 1,
+    HUD_WEAPON_ICON = 2,
+    HUD_CASH = 3,
+    HUD_MP_CASH = 4,
+    HUD_MP_MESSAGE = 5,
+    HUD_VEHICLE_NAME = 6,
+    HUD_AREA_NAME = 7,
+    HUD_VEHICLE_CLASS = 8,
+    HUD_STREET_NAME = 9,
+    HUD_HELP_TEXT = 10,
+    HUD_FLOATING_HELP_TEXT_1 = 11,
+    HUD_FLOATING_HELP_TEXT_2 = 12,
+    HUD_CASH_CHANGE = 13,
+    HUD_RETICLE = 14,
+    HUD_SUBTITLE_TEXT = 15,
+    HUD_RADIO_STATIONS = 16,
+    HUD_SAVING_GAME = 17,
+    HUD_GAME_STREAM = 18,
+    HUD_WEAPON_WHEEL = 19,
+    HUD_WEAPON_WHEEL_STATS = 20,
+    MAX_HUD_COMPONENTS = 21,
+    MAX_HUD_WEAPONS = 22,
+    MAX_SCRIPTED_HUD_COMPONENTS = 141
+}
 
 local CellFrontCamActivate = function(activate)
 	return Citizen.InvokeNative(0x2491A93618B7D838, activate)
@@ -26,11 +51,7 @@ function TakeVideo(data, cb)
 			CellCamActivate(false, false)
 		end
 
-		HideHudComponentThisFrame(7)
-		HideHudComponentThisFrame(8)
-		HideHudComponentThisFrame(9)
-		HideHudComponentThisFrame(6)
-		HideHudComponentThisFrame(19)
+		for _, id in pairs(HUD_ELEMENTS) do HideHudComponentThisFrame(id) end
 		HideHudAndRadarThisFrame()
 		Citizen.Wait(0)
 	end
@@ -79,11 +100,8 @@ function TakePhoto(data, cb)
 			end)
 		end
 
-		HideHudComponentThisFrame(7)
-		HideHudComponentThisFrame(8)
-		HideHudComponentThisFrame(9)
-		HideHudComponentThisFrame(6)
-		HideHudComponentThisFrame(19)
+		for _, id in pairs(HUD_ELEMENTS) do HideHudComponentThisFrame(id) end
 		HideHudAndRadarThisFrame()
+		Citizen.Wait(0)
 	end
 end
