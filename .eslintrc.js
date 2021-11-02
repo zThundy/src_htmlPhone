3b538949ade945b7e94468a8fdf3553794f9852f
zThundy <antonioantonelli7@gmail.com>
2021-11-02 23:39:59 +0100

    changes to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,7 @@ module.exports = {
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: [ 'html' ],
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
@@ -22,6 +20,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // ignore parentesys spaces after function like switch()
+    'keyword-spacing': "off"
   }
 }
